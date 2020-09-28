9ac08b2005b2b3b6bd219b7c6a248d86194a0c42
anquito <r-andg@hotmail.com>
2020-09-27 23:00:32 -0700

    Unit 3 Quiz

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,6 +35,14 @@ public class PlayerMovement : NetworkBehaviour
         direction = new Vector3(horMove, 0, verMove);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, direction * 10);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(transform.position, rbPlayer.velocity * 5);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
@@ -63,6 +71,7 @@ public class PlayerMovement : NetworkBehaviour
             index++;
         }
         rbPlayer.MovePosition(spawnPoints[index].transform.position);
+        rbPlayer.velocity = Vector3.zero;
     }
 
     private void OnTriggerExit(Collider other)
